d54608a4d336085becac599d34b71f4845aac08f
angelinawwu <angelinawwu@g.ucla.edu>
2025-05-08 16:52:41 -0700

    the mitochondria is the powerhouse of the cell

--- sketch.js
+++ sketch.js
@@ -1,32 +1,49 @@
-var rad;
+var radius;
 var res;
 var angle;
 var blobObj = []; // array of objects that holds blob attributes
+var yellow;
+var green;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    // Initialize colors inside setup
+    yellow = color(243, 255, 124);
+    green = color(6, 198, 1);
     textFont("Oranienbaum");
-    rad = 100; // radius of the circular path
-    res = 10; // the number of points 
+    radius = 200; // radius of the circular path
+    res = 12; // the number of points 
     angle = 360 / res; // angular distance between each point
     angleMode(DEGREES); // enable the Degree mode not to make calculations easier.
     background(255); // background color of the window
-    noLoop(); // Since we don't create and animation, we can disable the loop function
 }
 
 function draw() {
-    background(6, 198, 1);
+    background(green);
+
+    console.log("yes");
+    // Draw circle
+    push();
+    translate(width * 0.5 - 200, height * 0.5 - 200);
     noStroke();
-    fill(243, 255, 144);
+    fill(230, 120, 60);
+    circle(0, 0, radius);
+    filter(BLUR, 40);
+    pop();
+
+    // Draw blob
+    noStroke();
+    fill(yellow);
     push();
     translate(width * 0.5, height * 0.5);
     beginShape();
+    blobObj = [];
     for (var i = 0; i < res; i++) {
-        var randRad = min(rad, rad+random(-20, 20));
+        var randRadius = min(radius, radius+random(-50, 50));
         blobObj.push({
-         "rad": randRad,
-         "x": randRad * cos(angle * i),
-         "y": randRad * sin(angle * i)
+         "radius": randRadius,
+         "x": randRadius * cos(angle * i),
+         "y": randRadius * sin(angle * i)
         });
         circle(blobObj[i].x, blobObj[i].y, 0);
         curveVertex(blobObj[i].x, blobObj[i].y); // add points to the custom shape
@@ -35,11 +52,19 @@ function draw() {
     curveVertex(blobObj[1].x, blobObj[1].y);
     curveVertex(blobObj[2].x, blobObj[2].y);
     endShape();
+    filter(BLUR, 5);
     pop();
     
+    // Text
+    push();
+    fill(yellow);
+    stroke(0);
+    strokeWeight(2);
     textSize(50);
     textFont("Oranienbaum");
-    text("The mitochondria is the powerhouse of the cell", 210, 100);
+    textAlign(CENTER);
+    text("The mitochondria is the \npowerhouse of the cell", width/2, 100);
+    pop();
 
-    filter(BLUR, 1);
+    noLoop();
 }
