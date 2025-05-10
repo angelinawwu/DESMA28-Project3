9681553777c1f13fca7fc5dbfb648aa6bbbd2739
angelinawwu <angelinawwu@g.ucla.edu>
2025-05-10 15:21:26 -0700

    createGraphics

--- sketch.js
+++ sketch.js
@@ -1,59 +1,76 @@
 var radius;
-var res;
+var numPoints;
 var angle;
 var blobObj = []; // array of objects that holds blob attributes
 var yellow;
 var green;
-
+var orange;
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    // Initialize colors inside setup
-    yellow = color(243, 255, 124);
-    green = color(6, 198, 1);
+
+    // Initialize colors
+    yellow = color(214, 195, 39);
+    green = color(0, 148, 40);
+    orange = color(224, 123, 39);
+
     textFont("Oranienbaum");
-    radius = 200; // radius of the circular path
-    res = 12; // the number of points 
-    angle = 360 / res; // angular distance between each point
-    angleMode(DEGREES); // enable the Degree mode not to make calculations easier.
-    background(255); // background color of the window
+    radius = 200;
+    numPoints = 12;
+    angle = 360 / numPoints;
+    angleMode(DEGREES);
+    frameRate(10);
+
+    blurCircle = createGraphics(width, height);
+    blob = createGraphics(width, height);
 }
 
-function draw() {
-    background(green);
-
-    console.log("yes");
-    // Draw circle
-    push();
-    translate(width * 0.5 - 200, height * 0.5 - 200);
-    noStroke();
-    fill(230, 120, 60);
-    circle(0, 0, radius);
-    filter(BLUR, 40);
-    pop();
-
-    // Draw blob
-    noStroke();
-    fill(yellow);
-    push();
-    translate(width * 0.5, height * 0.5);
-    beginShape();
+function drawBlob(blobColor) {
+    blob.noStroke();
+    blob.fill(blobColor);
+    blob.push();
+    blob.translate(width * 0.5, height * 0.5);
+    blob.beginShape();
     blobObj = [];
-    for (var i = 0; i < res; i++) {
-        var randRadius = min(radius, radius+random(-50, 50));
+    for (let i = 0; i < numPoints; i++) {
+        let randRadius = min(radius, radius+random(-50, 50));
         blobObj.push({
          "radius": randRadius,
          "x": randRadius * cos(angle * i),
          "y": randRadius * sin(angle * i)
         });
-        circle(blobObj[i].x, blobObj[i].y, 0);
-        curveVertex(blobObj[i].x, blobObj[i].y); // add points to the custom shape
+        blob.curveVertex(blobObj[i].x, blobObj[i].y);
        }
-    curveVertex(blobObj[0].x, blobObj[0].y);
-    curveVertex(blobObj[1].x, blobObj[1].y);
-    curveVertex(blobObj[2].x, blobObj[2].y);
-    endShape();
-    filter(BLUR, 5);
-    pop();
+    blob.curveVertex(blobObj[0].x, blobObj[0].y);
+    blob.curveVertex(blobObj[1].x, blobObj[1].y);
+    blob.curveVertex(blobObj[2].x, blobObj[2].y);
+    blob.endShape();
+    blob.pop();
+    blob.filter(BLUR, 5);
+}
+
+function drawCircle() {
+    blurCircle.push();
+    blurCircle.translate(width * 0.5 - 100, height * 0.5 - 100);
+    blurCircle.noStroke();
+    blurCircle.fill(orange);
+    blurCircle.circle(0, 0, radius);
+    blurCircle.pop();
+    blurCircle.filter(BLUR, 20);
+}
+
+function draw() {
+    background(green);    
+
+    // Draw blob
+    drawBlob(yellow);
+    drawBlob(green);
+
+    // Draw orange circle
+    drawCircle();
+
+    image(blob, 0, 0);
+    image(blurCircle, 0, 0);
+
     
     // Text
     push();
@@ -66,5 +83,4 @@ function draw() {
     text("The mitochondria is the \npowerhouse of the cell", width/2, 100);
     pop();
 
-    noLoop();
 }
